4cc98926a7d18f817db0ea558cbead3e0a0fd2a8
Divya Sathish <divyasathish2204>
2023-11-10 12:46:16 +0530

    updated

---
+++ src/DimondPricePrediction/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        self.lineno = exc_tb.tb_lineno
+        self.filename=exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occured in python file[{0}] and in lineno [{1}],the error message is [{2}]".format(
+            self.filename,self.lineno,str(self.error_message))
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        raise CustomException(e,sys)
+    
---
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -0,0 +1,8 @@
+from src.DimondPricePrediction.components import DataIngestion
+import os
+import sys
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import CustomException
+import pandas as pd
+obj = DataIngestion()
+obj.initiate_data_ingestion()
