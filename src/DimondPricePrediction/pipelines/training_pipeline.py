f37a93abb568a8c85bfdd76ff2265e0fe15c77ad
Divya Sathish <divyasathish2204>
2023-11-20 10:24:47 +0530

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -8,7 +8,7 @@ from src.DimondPricePrediction.exception import CustomException
 import pandas as pd
 obj = DataIngestion()
 train_data_path,test_data_path=obj.initiate_data_ingestion()
-data_transformation = DataTransformation()
-train_arr,test_arr=data_transformation.initialize_datatransformation(train_data_path,test_data_path)
+obj1 = DataTransformation()
+train_arr,test_arr=obj1.initialize_datatransformation(train_data_path,test_data_path)
 model_trainer_obj = ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
