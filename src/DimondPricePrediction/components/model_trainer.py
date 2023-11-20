3091fc66949b94ce2b526df46c624389a9a6f26f
Divya Sathish <divyasathish2204>
2023-11-20 11:40:05 +0530

    update

--- src/DimondPricePrediction/components/model_trainer.py
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -1,67 +1,72 @@
-import os
-import sys
 import pandas as pd
 import numpy as np
+import os
+import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import CustomException
-from src.DimondPricePrediction.utils.utills import save_object
 from dataclasses import dataclass
+from src.DimondPricePrediction.utils.utills import save_object
 from src.DimondPricePrediction.utils.utills import evaluate_model
-from sklearn.linear_model import LinearRegression,Lasso,Ridge,ElasticNet
-@dataclass
-class Modeltrainerconfig():
-    trained_model_file_path = os.path.join("artifacts",'model.pkl')
 
-class ModelTrainer():
-    def _init_(self):
-        self.model_trainer_config = Modeltrainerconfig()
+from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 
+
+@dataclass 
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
     def initiate_model_training(self,train_array,test_array):
         try:
-            logging.info("spliting dependent and independent variables from train and test datasets")
-            x_train,y_train,x_test,y_test = (
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train, y_train, X_test, y_test = (
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
 
-            models = {
-                'Linearregression':LinearRegression(),
-                'Lasso' : Lasso(),
-                'Ridge' : Ridge(),
-                'Elastic_net' : ElasticNet()
-            }
-            model_report:dict = evaluate_model(x_train,y_train,x_test,y_test,models)
+            models={
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet()
+        }
+            
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
             print(model_report)
-            print("\n=====================================================\n")
-            logging.info(f"Model report:{model_report}")
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+
+            # To get best model score from dictionary 
             best_model_score = max(sorted(model_report.values()))
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
             best_model = models[best_model_name]
-            print(f"Best_model_name : {best_model_name} and R2 Score :{best_model_score}")
-            print('\n=========================================================\n')
-            logging.info("best model found :{best_model_name} and r2 score is {best_model_score}")
+
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
             save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj = best_model
+                 file_path=self.model_trainer_config.trained_model_file_path,
+                 obj=best_model
             )
+          
+
         except Exception as e:
-            logging.info("Exception occured while model training")
+            logging.info('Exception occured at Model Training')
             raise CustomException(e,sys)
 
-
-
-
-
-
-
-
-
-
-
-
-
+        
+    
 
 
 
