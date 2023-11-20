fa23adfaec165149643ddf828349495c2aace811
Divya Sathish <divyasathish2204>
2023-11-20 11:19:58 +0530

    updated 6

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -21,6 +21,6 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 
 model_trainer_obj=ModelTrainer()
-model_trainer_obj.initate_model_training(train_arr,test_arr)
+model_trainer_obj.initiate_model_training(train_arr,test_arr)
 
 
